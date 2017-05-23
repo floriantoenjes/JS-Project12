5995129808df23230b04a648502f1aaecab55964
Florian Tönjes <skatepainter@yahoo.de>
2017-05-23 10:52:01 +0200

    Add login template and controller

--- public/scripts/route-config.js
+++ public/scripts/route-config.js
@@ -3,9 +3,16 @@
 angular.module("app").config(config);
 
 function config($routeProvider) {
-    $routeProvider.when("/", {
+    $routeProvider
+    .when("/", {
         controller: "SearchController",
         controllerAs: "vm",
         templateUrl: "templates/search.html"
+    })
+    .when("/login", {
+       controller: "LoginController",
+        controllerAs: "vm",
+        templateUrl: "templates/login.html"
     });
+
 }
