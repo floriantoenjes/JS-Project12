0c4800755aac0a3e8a7ff61606e7bfd5c65fb5b8
Florian Tönjes <skatepainter@yahoo.de>
2017-05-23 11:52:06 +0200

    User registration working

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,15 @@ const router = express.Router();
 const User = require("../models/user");
 
 
-router.get("/register", function (req, res, next) {
-
+router.post("/register", function (req, res, next) {
+    const user = new User(req.body);
+    user.save(function (error, user) {
+        if (error) {
+            return next(error);
+        }
+        res.status(201);
+        res.send();
+    })
 });
 
 
