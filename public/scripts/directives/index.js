906d2f541fd9707ac90411a78a844daea02a8367
Florian Tönjes <skatepainter@yahoo.de>
2017-05-24 10:24:23 +0200

    Replaced autofocus directive

--- public/scripts/directives/index.js
+++ public/scripts/directives/index.js
@@ -1,17 +1,23 @@
-angular.module('app')
-    .directive('focus', function ($timeout) {
+angular.module("app")
+    .directive('focusMe', ['$timeout', '$parse', function ($timeout, $parse) {
         return {
-            scope: {
-                trigger: '@focus'
-            },
-            link: function (scope, element) {
-                scope.$watch('trigger', function (value) {
-                    if (value === "true") {
+            //scope: true,   // optionally create a child scope
+            link: function (scope, element, attrs) {
+                var model = $parse(attrs.focusMe);
+                scope.$watch(model, function (value) {
+                    if (value === true) {
                         $timeout(function () {
                             element[0].focus();
                         });
                     }
                 });
+                // to address @blesh's comment, set attribute value to 'false'
+                // on blur event:
+                element.bind('blur', function () {
+                    if (model.assign !== undefined) {
+                        scope.$apply(model.assign(scope, false));
+                    }
+                });
             }
         };
-    });
+    }]);
