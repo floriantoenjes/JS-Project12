95f056aa522f06b6355394f41212e793cc6ee270
Florian Tönjes <skatepainter@yahoo.de>
2017-05-24 14:29:41 +0200

    Catch empty query string

--- public/scripts/controllers/search.js
+++ public/scripts/controllers/search.js
@@ -5,6 +5,9 @@ angular.module("app")
     .controller("SearchController", function ($route, $scope, authenticationService, dataService) {
 
         $scope.search = function (query) {
+            if (query === undefined || query.trim().length === 0) {
+                return;
+            }
             dataService.getMovieSoundtrack(query, function (response) {
                 $scope.results = response.data;
             });
