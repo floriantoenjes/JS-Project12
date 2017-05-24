458e459542132d6432e387379fc031fbd5ba79a0
Florian Tönjes <skatepainter@yahoo.de>
2017-05-24 09:22:53 +0200

    Implemented validation for password fields

--- public/scripts/controllers/user.js
+++ public/scripts/controllers/user.js
@@ -15,20 +15,20 @@ angular.module("app")
 
     $scope.register = function (user) {
         if (!user) {
-            const error = new Error()
+            const error = new Error();
             error.message = "Enter an email address.";
             return $scope.error = error;
 
         }
 
         if (!user.password) {
-            const error = new Error()
+            const error = new Error();
             error.message = "Enter a password.";
             return $scope.error = error;
         }
 
         if (user.password !== user.confirmPassword) {
-            const error = new Error()
+            const error = new Error();
             error.message = "Passwords do not match.";
             return $scope.error = error;
         }
@@ -38,8 +38,11 @@ angular.module("app")
         };
         authenticationService.register(userObject, function (error) {
             if (error) {
+                console.log("Error", error);
                 if (error.data.errors.email) {
-                    error.message = "Please enter a valid Email address";
+                    error.message = "Please enter a valid email address";
+                } else if (error.data.errors.password) {
+                    error.message = "Password has to be between 4 and 25 characters."
                 }
                 return $scope.error = error;
             }
--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,9 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 4,
+        maxlength: 25
     }
 });
 
