4c99372f345f78d94fd8338872527205ef0d5922
Florian Tönjes <skatepainter@yahoo.de>
2017-05-25 16:42:11 +0200

    Add Soundtrack model

--- src/models/soundtrack.js
+++ src/models/soundtrack.js
@@ -1,5 +1,25 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const SoundtrackSchema = new mongoose.Schema({
-    spotifyId: {}
+    _id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    },
+    imageUrl: String
+
 });
+
+SoundtrackSchema.plugin(uniqueValidator);
+
+const Soundtrack = mongoose.model("Soundtrack", SoundtrackSchema);
+module.exports = Soundtrack;
