158474f15c981120bea2e06e50e7f11766664c69
Florian Tönjes <skatepainter@yahoo.de>
2017-05-27 00:24:45 +0200

    Refactored first tests

--- test/api_test.js
+++ test/api_test.js
@@ -7,6 +7,13 @@ describe("user routes", function () {
     let server;
     let token = "";
 
+    const usersPath = "/api/v1/users";
+
+    const testUser = {
+        email: "testuser@test.com",
+        password: "password"
+    };
+
     before(function (done) {
         server = require("../src/index");
 
@@ -16,11 +23,8 @@ describe("user routes", function () {
             }
 
             request(server)
-                .post("/api/v1/users/register")
-                .send({
-                    email: "testuser@test.com",
-                    password: "password"
-                })
+                .post(`${usersPath}/register`)
+                .send(testUser)
                 .end(function (error, res) {
                     const result = JSON.parse(res.text);
                     token = result.token;
@@ -39,18 +43,21 @@ describe("user routes", function () {
 
     it("should login", function (done) {
         request(server)
-            .post("/api/v1/users/login")
-            .send({
-                email: "testuser@test.com",
-                password: "password"
-            })
+            .post(`${usersPath}/login`)
+            .send(testUser)
             .expect(200)
             .expect("Content-Type", "application/json; charset=utf-8", done);
     });
 
+    it("should not return all users but unauthorized", function (done) {
+        request(server)
+            .get(`${usersPath}`)
+            .expect(401, done);
+    });
+
     it("should get all users", function (done) {
         request(server)
-            .get("/api/v1/users")
+            .get(`${usersPath}`)
             .set("Authorization", "Bearer " + token)
             .expect(200)
             .expect("Content-Type", "application/json; charset=utf-8", done);
