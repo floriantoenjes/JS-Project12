d7a166887bbfd4a4cc82cd4c25361dde0d726fb3
Florian Tönjes <skatepainter@yahoo.de>
2017-05-28 21:48:26 +0200

    First own test running

--- test/frontend-tests/test.js
+++ test/frontend-tests/test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('app')
-    .controller('MyCtrl', ['$scope', function MyCtrl($scope) {
-        $scope.greeting = "hello";
-    }]);
+// angular.module('app')
+//     .controller('MyCtrl', ['$scope', function MyCtrl($scope) {
+//         $scope.greeting = "hello";
+//     }]);
 
 describe('My controller', function() {
     var $controller, $scope;
@@ -16,10 +16,10 @@ describe('My controller', function() {
     }));
 
     it('greets', function() {
-        var controller = $controller('MyCtrl', {
+        var controller = $controller('UsersController', {
             $scope: $scope
         });
-        expect($scope.greeting).toEqual('hello');
+        dump("USERS", $scope.users);
     })
 
 });
