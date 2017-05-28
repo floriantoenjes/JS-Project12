c6d4e151bb0d9eec37607967d0e7d24d6106cba4
Florian Tönjes <skatepainter@yahoo.de>
2017-05-28 22:50:28 +0200

    First real frontend test working

---
+++ test/frontend-tests/UserControllerTest.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('UsersController', function () {
+    var $controller, $scope;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function ($rootScope, _$controller_) {
+        $controller = _$controller_;
+        $scope = $rootScope.$new();
+    }));
+
+    it("should initialize users", inject(function ($httpBackend) {
+        var controller = $controller('UsersController', {
+            $scope: $scope
+        });
+        $httpBackend.whenGET("/api/v1/users").respond([
+            {"_id": "5929b5d7475e8123f4fda449", "email": "user2@test.com"},
+            {"_id": "5929b5d7475e8123f4fda44a", "email": "user@test.com"}
+            ]);
+        $httpBackend.flush();
+
+        $scope.$digest();
+
+        expect($scope.users.length).toBe(2);
+    }));
+});
