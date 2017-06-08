0032156f7f1f466078135a76f48f16b1850ae6f3
Florian Tönjes <skatepainter@yahoo.de>
2017-06-08 10:40:08 +0200

    Search disabled when searching now

--- public/scripts/controllers/search.js
+++ public/scripts/controllers/search.js
@@ -23,15 +23,22 @@ angular.module("app")
 
         // Functions
         $scope.search = function (query) {
+            $scope.disableSearch = true;
+
             if (query === undefined || query.trim().length === 0) {
+                $scope.disableSearch = false;
                 return;
             }
             dataService.getMovieSoundtrack(query, function (error, response) {
                 if (error) {
                     const error = new Error();
                     error.message = `No results for ${query}.`;
+
+                    $scope.disableSearch = false;
                     return $scope.error = error;
                 }
+
+                $scope.disableSearch = false;
                 $scope.error = false;
                 $scope.results = response.data;
             });
